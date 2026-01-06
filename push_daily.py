6481fca4c2534399a95bf66c13f4d41700e6673d
Stefan Ivchenko <sivc@kth.se>
2026-01-06 10:51:16 +0100

    new error

--- push_daily.py
+++ push_daily.py
@@ -19,9 +19,12 @@ fs = project.get_feature_store(name='birdup_featurestore')
 def insert_daily_data():
     # retrive todays data: 
     birding_fg = fs.get_feature_group(name='birding',version=1,)
-    print(birding_fg)
     daily_df = df_functions.daily()
-    df_prev = birding_fg.read()
+    print(type(birding_fg.features[0]), birding_fg.features[0])
+
+    cols = [f["name"] for f in birding_fg.features]
+    df_prev = birding_fg.select(cols).read()
+
     daily_df = df_functions.to_hopsworks_df(daily_df)
     day_df_lag = df_functions.add_daily_lags_from_hopsworks_simple(daily_df, df_prev, k=5)
     birding_fg.insert(day_df_lag, wait=True)
